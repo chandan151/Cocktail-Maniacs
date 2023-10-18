43574001d754b6201163a8f9c7e461ea58c36143
Chandan sharma <c.sharma@uleth.ca>
2023-10-18 03:39:42 -0600

    Finally some progress with fetching data

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-console.log('Hello World!');
+/* Javascript file*/
 
 const xhr = new XMLHttpRequest();
 
 const baseurl = "https://wizard-world-api.herokuapp.com";
-const query = "/Houses";
-const url = baseurl + query;
+const query1 = "/Houses";
+const query2 = "/Elixirs";
+const url = baseurl + query2;
 var harryPotterHouses;
+var harryPotterElixirs;
 var index = 0;
 
 xhr.open("GET", url);
@@ -13,9 +15,55 @@ xhr.send(null);
 
 xhr.onload = function() {
     console.log("hurray, it worked!");
-    //console.log(xhr.responseText);
-    harryPotterHouses = JSON.parse(xhr.responseText);
-    console.log(harryPotterHouses);
+    harryPotterElixirs = JSON.parse(xhr.responseText);
+    //console.log(harryPotterElixirs);
 
-    harry.innerHTML = "The name of house is " + harryPotterHouses[index].name + " and the founder is " + harryPotterHouses[index].founder + " and the head of house is " + harryPotterHouses[index].heads[1].firstName + " and the house ghost is " + harryPotterHouses[index].houseGhost + " and the house colors are " + harryPotterHouses[index].colors + " and the house mascot is " + harryPotterHouses[index].mascot + " and the house values are " + harryPotterHouses[index].values + " and the house members are " + harryPotterHouses[index].members + " and the house id is " + harryPotterHouses[index].id;
+    printElixirs();
+    printIngredients();
+    printInventors();
 }
+
+function printElixirs() {
+    var elixir = document.getElementById("elixir");
+    elixir.innerHTML = "The name of the elixir is " + harryPotterElixirs[index].name + " and the ingredients are " + harryPotterElixirs[index].ingredients[0].name + " and the price is " + harryPotterElixirs[index].price + " and the id is " + harryPotterElixirs[index].id;
+}
+
+function printIngredients() {
+
+    var text = [];
+    for (let i = 0; i < harryPotterElixirs.length; i++) {
+        for (let j = 0; j < harryPotterElixirs[i].ingredients.length; j++) {
+            //text += harryPotterElixirs[i].ingredients[j].name + ",   ";
+            text.push(" " + harryPotterElixirs[i].ingredients[j].name);
+        }    
+    }
+    text.sort();
+    text = removeDuplicates(text);
+    //console.log(text);
+
+    var ingredients = document.getElementById("ingredients");
+    ingredients.innerHTML = text;
+}
+
+function printInventors() {
+
+    var text = [];
+    for (let i = 0; i < harryPotterElixirs.length; i++) {
+        for (let j = 0; j < harryPotterElixirs[i].inventors.length; j++) {
+            text.push(" " + harryPotterElixirs[i].inventors[j].firstName + " " + harryPotterElixirs[i].inventors[j].lastName);
+        }    
+    }
+    text.sort();
+    text = removeDuplicates(text);
+    //console.log(text);
+
+    var inventors = document.getElementById("inventors");
+    inventors.innerHTML = text;
+}
+
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+  }
+
+
+// harry.innerHTML = "The name of house is " + harryPotterHouses[index].name + " and the founder is " + harryPotterHouses[index].founder + " and the head of house is " + harryPotterHouses[index].heads[1].firstName + " and the house ghost is " + harryPotterHouses[index].houseGhost + " and the house colors are " + harryPotterHouses[index].colors + " and the house mascot is " + harryPotterHouses[index].mascot + " and the house values are " + harryPotterHouses[index].values + " and the house members are " + harryPotterHouses[index].members + " and the house id is " + harryPotterHouses[index].id;
