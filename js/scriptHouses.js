0b872f10266cf23305a810659bee5d873711a012
Chandan sharma <c.sharma@uleth.ca>
2023-10-19 11:25:22 -0600

    added functionality of locations button

--- js/scriptHouses.js
+++ js/scriptHouses.js
@@ -6,7 +6,7 @@ const baseurl1 = "https://wizard-world-api.herokuapp.com";
 const query1 = "/Houses";
 const url1 = baseurl1 + query1;
 var harryPotterHouses;
-var index = 0;
+var count = 0;
 
 xhr1.open("GET", url1);
 xhr1.send(null);
@@ -34,7 +34,11 @@ myButton1.addEventListener("click", function() {
 
 function printHouses() {
   var houses = document.getElementById("showcase");
-  //houses.innerHTML = (index+1) + "Name of the store is " + harryPotterHouses[index].name + ", that was founded by " + harryPotterHouses[index].founder + ""
-
-  houses.innerHTML = "The name of Store is " + harryPotterHouses[index].name + " and the founder is " + harryPotterHouses[index].founder + " and the head of Store is " + harryPotterHouses[index].heads[1].firstName + " and the store colors are " + harryPotterHouses[index].colors
+  text = "";
+    for (let count = 0; count < harryPotterHouses.length; count++) {
+       text += "<br>" + " Store "+ (count+1) + ": " + harryPotterHouses[count].name + ", It is managed by " + harryPotterHouses[count].founder + ". The head cook of this store is " + harryPotterHouses[count].heads[0].firstName + " " + harryPotterHouses[count].heads[0].lastName + ". It is located in " + harryPotterHouses[count].commonRoom + " that is colured in " + harryPotterHouses[count].houseColours + " Colors and is guarded by " + harryPotterHouses[count].ghost + " and " + harryPotterHouses[count].animal + ". The main element of this store is " + harryPotterHouses[count].element + "<br>"
+    }
+  houses.innerHTML = text;
 }
+
+/*houses.innerHTML = "The name of Store is " + harryPotterHouses[index].name + " and the founder is " + harryPotterHouses[index].founder + " and the head of Store is " + harryPotterHouses[index].heads[1].firstName + " and the store colors are " + harryPotterHouses[index].colors*/
